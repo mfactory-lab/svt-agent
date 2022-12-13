7dd4bc7755fe743613d48b38e13a1f468d4f5779
tiamo <vk.tiamo@gmail.com>
2022-12-13 02:50:31 +0200

    fix: new test channel id

--- src/constants.rs
+++ src/constants.rs
@@ -4,7 +4,7 @@
 // New messenger program
 pub const MESSENGER_PROGRAM_ID: &str = "4AnSBTc21f4wTBHmnFyarbosr28Qk4CgGFBHcRh4kYPw";
 
-pub const DEFAULT_CHANNEL_ID: &str = "5iQBPWjiYZTMmw254YKkhi2zPSKtyQXhShVX1zjfYEU5";
+pub const DEFAULT_CHANNEL_ID: &str = "Ao4HVCWGyxBSBzT4L9LTLfvjLdHs6MccGVTsYUg6vEt3";
 
 // restart_validator<!>-a 1,-b 2<!>secret123
 pub const COMMAND_DELIMITER: &str = "<!>";
