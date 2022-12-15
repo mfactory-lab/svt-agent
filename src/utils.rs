4882ecc8aaf63849c9417a7c034061acda32b0c6
tiamo <vk.tiamo@gmail.com>
2022-12-15 05:14:56 +0200

    feat: added `set_last_read_message_id`

--- src/utils.rs
+++ src/utils.rs
@@ -1,11 +1,20 @@
-use crate::constants::COMMAND_DELIMITER;
+use crate::constants::{COMMAND_DELIMITER, MESSENGER_PROGRAM_ID};
 use crate::encryption::decrypt_message;
 use crate::runner::Task;
-use crate::state::Message;
+use anchor_client::solana_client::nonblocking::rpc_client::RpcClient;
+use anchor_client::solana_sdk::instruction::{AccountMeta, Instruction};
+use anchor_client::solana_sdk::message::Message;
+use anchor_client::solana_sdk::signature::{Keypair, Signature};
+use anchor_client::solana_sdk::signer::Signer;
+use anchor_client::solana_sdk::system_program;
+use anchor_client::solana_sdk::transaction::Transaction;
+use anchor_lang::prelude::borsh::{BorshDeserialize, BorshSerialize};
+use anchor_lang::prelude::*;
 use anyhow::Error;
 use anyhow::Result;
+use std::str::FromStr;
 
-pub fn convert_message_to_task(msg: Message, cek: &[u8]) -> Result<Task> {
+pub fn convert_message_to_task(msg: crate::state::Message, cek: &[u8]) -> Result<Task> {
     let cmd = String::from_utf8(decrypt_message(msg.content, cek)?)?;
     let parts = cmd.split(COMMAND_DELIMITER).collect::<Vec<_>>();
 
@@ -24,4 +33,45 @@ pub fn convert_message_to_task(msg: Message, cek: &[u8]) -> Result<Task> {
         playbook,
         extra_vars,
     })
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+struct ReadMessage {
+    pub message_id: u64,
+}
+
+pub async fn set_last_read_message_id(
+    client: &RpcClient,
+    task: Task,
+    channel: &Pubkey,
+    membership: &Pubkey,
+    authority: &Keypair,
+) -> Result<Signature> {
+    let program_id = Pubkey::from_str(MESSENGER_PROGRAM_ID)?;
+
+    let account_metas = vec![
+        AccountMeta::new(*channel, false),
+        AccountMeta::new(*membership, false),
+        AccountMeta::new_readonly(authority.pubkey(), true),
+        AccountMeta::new_readonly(system_program::id(), false),
+    ];
+
+    let message = Message::new(
+        &[Instruction::new_with_borsh(
+            program_id,
+            &ReadMessage {
+                message_id: task.id,
+            },
+            account_metas,
+        )],
+        Some(&authority.pubkey()),
+    );
+
+    let mut tx = Transaction::new_unsigned(message);
+    let blockhash = client.get_latest_blockhash().await?;
+    tx.sign(&[authority], blockhash);
+
+    let sig = client.send_and_confirm_transaction(&tx).await?;
+
+    Ok(sig)
 }
