4b5c91acf7537b8823080c97460cd83dfc8c1a13
tiamo <vk.tiamo@gmail.com>
2022-12-19 23:01:36 +0200

    feat: added `MessengerClient`

---
+++ src/utils/messenger.rs
@@ -0,0 +1,163 @@
+use crate::encryption::decrypt_cek;
+use crate::state::*;
+use anchor_client::solana_client::nonblocking::rpc_client::RpcClient;
+use anchor_client::solana_sdk::commitment_config::CommitmentConfig;
+use anchor_client::solana_sdk::instruction::{AccountMeta, Instruction};
+use anchor_client::solana_sdk::message::Message;
+use anchor_client::solana_sdk::signature::{Keypair, Signature};
+use anchor_client::solana_sdk::signer::Signer;
+use anchor_client::solana_sdk::system_program;
+use anchor_client::solana_sdk::transaction::Transaction;
+use anchor_client::Cluster;
+use anchor_lang::prelude::*;
+use anyhow::Result;
+use tracing::info;
+
+const AGENT_NAME: &[u8] = b"agent";
+
+pub struct MessengerClient {
+    pub program_id: Pubkey,
+    pub rpc: RpcClient,
+    pub cluster: Cluster,
+    pub authority: Keypair,
+}
+
+impl MessengerClient {
+    pub fn new(cluster: Cluster, program_id: Pubkey, authority: Keypair) -> Self {
+        let rpc = RpcClient::new_with_commitment(
+            cluster.url().to_string(),
+            CommitmentConfig::confirmed(),
+        );
+
+        Self {
+            rpc,
+            cluster,
+            program_id,
+            authority,
+        }
+    }
+
+    #[tracing::instrument(skip(self))]
+    pub async fn join_channel(&self, channel: &Pubkey) -> Result<Signature> {
+        let authority = self.authority.pubkey();
+        let membership = self.membership_pda(channel, &authority);
+
+        let device = self.device_pda(&membership.0, &authority);
+
+        let account_metas = vec![
+            AccountMeta::new(*channel, false),
+            AccountMeta::new(membership.0, false),
+            AccountMeta::new(device.0, false),
+            AccountMeta::new(authority, true),
+            AccountMeta::new_readonly(authority, true),
+            AccountMeta::new_readonly(system_program::id(), false),
+        ];
+
+        let discriminator: &[u8] = &[124, 39, 115, 89, 217, 26, 38, 29];
+
+        let message = Message::new(
+            &[Instruction::new_with_bytes(
+                self.program_id,
+                &[discriminator, AGENT_NAME].concat(),
+                account_metas,
+            )],
+            Some(&authority),
+        );
+
+        let sig = self.send_transaction(message, None).await?;
+
+        Ok(sig)
+    }
+
+    #[tracing::instrument(skip(self))]
+    pub async fn read_message(&self, message_id: u64, channel: &Pubkey) -> Result<Signature> {
+        let authority = self.authority.pubkey();
+        let membership = self.membership_pda(channel, &authority);
+
+        let account_metas = vec![
+            AccountMeta::new(*channel, false),
+            AccountMeta::new(membership.0, false),
+            AccountMeta::new_readonly(authority, true),
+            AccountMeta::new_readonly(system_program::id(), false),
+        ];
+
+        let discriminator: &[u8] = &[54, 166, 48, 51, 234, 46, 110, 163];
+
+        let message = Message::new(
+            &[Instruction::new_with_bytes(
+                self.program_id,
+                &[discriminator, &message_id.to_le_bytes()].concat(),
+                account_metas,
+            )],
+            Some(&authority),
+        );
+
+        let sig = self.send_transaction(message, None).await?;
+
+        Ok(sig)
+    }
+
+    #[tracing::instrument(skip(self))]
+    pub async fn load_cek(&self, channel: &Pubkey) -> Result<Vec<u8>> {
+        info!("Loading device...");
+        let device = self.load_device(channel).await?;
+        info!("Decrypting CEK...");
+        decrypt_cek(device.cek, self.authority.secret().as_bytes())
+    }
+
+    #[tracing::instrument(skip(self))]
+    pub async fn load_membership(&self, channel: &Pubkey) -> Result<ChannelMembership> {
+        let authority = &self.authority.pubkey();
+        let pda = self.membership_pda(channel, authority);
+        self.load_account(&pda.0).await
+    }
+
+    #[tracing::instrument(skip(self))]
+    pub async fn load_device(&self, channel: &Pubkey) -> Result<ChannelDevice> {
+        let authority = &self.authority.pubkey();
+        let membership_pda = self.membership_pda(channel, authority);
+        let pda = self.device_pda(&membership_pda.0, authority);
+        self.load_account(&pda.0).await
+    }
+
+    #[tracing::instrument(skip(self))]
+    pub async fn load_channel(&self, channel: &Pubkey) -> Result<Channel> {
+        self.load_account(channel).await
+    }
+
+    pub async fn load_account<T: AnchorDeserialize>(&self, addr: &Pubkey) -> Result<T> {
+        let data = self.rpc.get_account_data(addr).await?;
+        // skip anchor discriminator
+        let account = T::deserialize(&mut &data.as_slice()[8..])?;
+        Ok(account)
+    }
+
+    pub fn membership_pda(&self, channel: &Pubkey, authority: &Pubkey) -> (Pubkey, u8) {
+        Pubkey::find_program_address(
+            &[&channel.to_bytes(), &authority.to_bytes()],
+            &self.program_id,
+        )
+    }
+
+    pub fn device_pda(&self, membership: &Pubkey, key: &Pubkey) -> (Pubkey, u8) {
+        Pubkey::find_program_address(&[&membership.to_bytes(), &key.to_bytes()], &self.program_id)
+    }
+
+    async fn send_transaction(
+        &self,
+        message: Message,
+        signers: Option<Vec<&Keypair>>,
+    ) -> Result<Signature> {
+        let mut tx = Transaction::new_unsigned(message);
+        let blockhash = self.rpc.get_latest_blockhash().await?;
+
+        tx.sign(&[&self.authority], blockhash);
+        if let Some(signers) = signers {
+            tx.partial_sign(&signers, blockhash);
+        }
+
+        let sig = self.rpc.send_and_confirm_transaction(&tx).await?;
+
+        Ok(sig)
+    }
+}
---
+++ src/utils/mod.rs
@@ -0,0 +1,28 @@
+pub mod messenger;
+
+use crate::constants::COMMAND_DELIMITER;
+use crate::encryption::decrypt_message;
+use crate::runner::Task;
+use anyhow::Error;
+use anyhow::Result;
+
+pub fn convert_message_to_task(msg: crate::state::Message, cek: &[u8]) -> Result<Task> {
+    let cmd = String::from_utf8(decrypt_message(msg.content, cek)?)?;
+    let parts = cmd.split(COMMAND_DELIMITER).collect::<Vec<_>>();
+
+    if parts.len() < 3 {
+        return Err(Error::msg("Invalid command"));
+    }
+
+    let playbook = String::from(parts[0]);
+    // TODO: validate
+    let extra_vars = String::from(parts[1]);
+    let uuid = String::from(parts[2]);
+
+    Ok(Task {
+        id: msg.id,
+        uuid,
+        playbook,
+        extra_vars,
+    })
+}
