44a79d7f068f4c808c30fc788b9bcf38f9f977ec
tiamo <vk.tiamo@gmail.com>
2023-01-06 03:26:26 +0200

    fix: `install` script

--- install.sh
+++ install.sh
@@ -1,134 +1,113 @@
-#!/bin/sh
-# shellcheck shell=dash
+#!/bin/bash
+#set -e
 
-# This is just a little script that can be downloaded from the internet to
-# install svt-agent. It just does platform detection, downloads the installer
-# and runs it.
+#
+# SVT Agent installation script
+#
+# Run command:
+#   CHANNEL_ID=QXK7qRCaabreGjcNHcKEadQDtTY9BJKHKfU11QAE6Xp CLUSTER=devnet \
+#   sh -c "$(curl -sSfL https://svt.one/install-agent.sh)"
+#
 
-{ # this ensures the entire script is downloaded #
+AGENT_RELEASE="${AGENT_RELEASE:-latest}"
+CLUSTER="${CLUSTER:-devnet}"
+CONTAINER_NAME="${CONTAINER_NAME:-svt-agent}"
+EXPOSE_PORT="${EXPOSE_PORT:-8888}" # Port is used to view task logs
+SSHKEY_PATH="$HOME/.ssh/svt-agent"
+WORKING_DIR="$HOME/svt-agent"
+KEYPAIR_PATH="$WORKING_DIR/authority.json"
 
-DOWNLOAD_ROOT="${DOWNLOAD_ROOT:-https://github.com/mfactory-lab/svt-agent/releases/download/}"
-GH_LATEST_RELEASE="https://api.github.com/repos/mfactory-lab/svt-agent/releases/latest"
-BIN_NANE=svt-agent-install
-#AGENT_RELEASE=
+# ARE YOU ROOT (or sudo)?
+#if [[ $EUID -ne 0 ]]; then
+#	echo -e "ERROR: This script must be run as root"
+#	exit 1
+#fi
 
-set -e
+do_install() {
+  echo "Installing SVT Agent..."
 
-usage() {
-    cat 1>&2 <<EOF
-$BIN_NANE
-initializes a new installation
+  ensure is_valid_cluster $CLUSTER
+  # TODO: validate channel_id
+  # ensure is_valid_channel $CHANNEL_ID
+  ensure generate_sshkey "id_rsa"
 
-USAGE:
-    $BIN_NANE [FLAGS] [OPTIONS] --data_dir <PATH> --pubkey <PUBKEY>
+  if [ -z "$CHANNEL_ID" ]; then
+    err "Please provide \"CHANNEL_ID\""
+  fi
 
-FLAGS:
-    -h, --help              Prints help information
-        --no-modify-path    Don't configure the PATH environment variable
+  if ! check_cmd "docker"; then
+    echo "Installing docker..."
+    sh -c "$(curl -fsSL https://get.docker.com)"
+    sudo systemctl start docker && sudo systemctl enable docker
+    echo "Done"
+  fi
 
-OPTIONS:
-    -d, --data_dir <PATH>    Directory to store install data
-    -u, --url <URL>          JSON RPC URL for the solana cluster
-    -p, --pubkey <PUBKEY>    Public key of the update manifest
-EOF
+  if [ ! -d $WORKING_DIR ]; then
+    mkdir -p $WORKING_DIR
+  fi
+
+  #say "Setup firewall..."
+  #sudo ufw allow $EXPOSE_PORT/tcp
+  #say "Done"
+
+  say "Downloading agent image (release: $AGENT_RELEASE)..."
+
+#  ensure docker pull ghcr.io/mfactory-lab/svt-agent:$AGENT_RELEASE
+  docker stop $CONTAINER_NAME 2>/dev/null 1>/dev/null
+  docker container rm $CONTAINER_NAME 2>/dev/null 1>/dev/null
+
+  # Generate agent keypair
+  if [[ -f $KEYPAIR_PATH ]]; then
+    say "Agent keypair already exits ($KEYPAIR_PATH)"
+  else
+    KEYPAIR="$(docker run --rm -it mfactory-lab/svt-agent:$AGENT_RELEASE generate-keypair)"
+    echo $KEYPAIR > $KEYPAIR_PATH
+    say "Added new agent keypair... ($KEYPAIR_PATH)"
+  fi
+
+  if [[ ! -f $KEYPAIR_PATH ]]; then
+    err "Something went wrong. Agent keypair file is not exists."
+  fi
+
+  # Run agent
+  say "Starting docker container..."
+  CONTAINER_ID="$(docker run -d -it --restart=always --name $CONTAINER_NAME \
+    --hostname $CONTAINER_NAME \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v svt-agent-ansible:/app/ansible \
+    -v $SSHKEY_PATH:/root/.ssh \
+    -v $KEYPAIR_PATH:/app/keypair.json \
+    -p $EXPOSE_PORT:8888 \
+    mfactory-lab/svt-agent:$AGENT_RELEASE \
+    run \
+    --cluster $CLUSTER \
+    -c $CHANNEL_ID)"
+
+  say "Done"
 }
 
-main() {
-    downloader --check
-    need_cmd uname
-    need_cmd mktemp
-    need_cmd chmod
-    need_cmd mkdir
-    need_cmd rm
-    need_cmd sed
-    need_cmd grep
+generate_sshkey() {
+  if [[ -f $SSHKEY_PATH/$@ ]]; then
+      say "SSH Keyfile already exists - skipping"
+  else
+      mkdir -p $SSHKEY_PATH
+      ssh-keygen -t rsa -b 4096 -f $SSHKEY_PATH/$@ -q -N '' -C 'svt-agent'
+      chmod 600 $SSHKEY_PATH/$@
+      cat $SSHKEY_PATH/$@.pub >> ~/.ssh/authorized_keys
+      say "SSH Keyfile was generated"
+  fi
+}
 
-    for arg in "$@"; do
-      case "$arg" in
-        -h|--help)
-          usage
-          exit 0
-          ;;
-        *)
-          ;;
-      esac
-    done
+is_valid_cluster() {
+  if [[ "$@" =~ ^(mainnet-beta|devnet|testnet)$ ]]; then
+    say "Cluster: $@"
+  else
+    err "Invalid cluster \"$@\""
+  fi
+}
 
-    _ostype="$(uname -s)"
-    _cputype="$(uname -m)"
-
-    case "$_ostype" in
-    Linux)
-      _ostype=unknown-linux-gnu
-      ;;
-    Darwin)
-      if [[ $_cputype = arm64 ]]; then
-        _cputype=aarch64
-      fi
-      _ostype=apple-darwin
-      ;;
-    *)
-      err "machine architecture is currently unsupported"
-      ;;
-    esac
-    TARGET="${_cputype}-${_ostype}"
-
-    _ansi_escapes_are_valid=false
-    if [ -t 2 ]; then
-        if [ "${TERM+set}" = 'set' ]; then
-            case "$TERM" in
-                xterm*|rxvt*|urxvt*|linux*|vt*)
-                    _ansi_escapes_are_valid=true
-                ;;
-            esac
-        fi
-    fi
-
-    _dir="$(ensure mktemp -d)"
-
-    # Check for $AGENT_RELEASE environment variable override.  Otherwise fetch
-    # the latest release tag from github
-    if [ -n "$AGENT_RELEASE" ]; then
-      release="$AGENT_RELEASE"
-    else
-      release_file="$_dir/release"
-      printf 'looking for latest release\n' 1>&2
-      ensure downloader "$GH_LATEST_RELEASE" "$release_file"
-      release=$(\
-        grep -m 1 \"tag_name\": "$release_file" \
-        | sed -ne 's/^ *"tag_name": "\([^"]*\)",$/\1/p' \
-      )
-      if [ -z "$release" ]; then
-        err 'Unable to figure latest release'
-      fi
-    fi
-
-    _url="$DOWNLOAD_ROOT/$release/$BIN_NANE-$TARGET"
-    _file="$_dir/$BIN_NANE"
-
-    if $_ansi_escapes_are_valid; then
-        printf "\33[1minfo:\33[0m downloading installer\n" 1>&2
-    else
-        printf '%s\n' 'info: downloading installer' 1>&2
-    fi
-
-    ensure mkdir -p "$_dir"
-    ensure downloader "$_url" "$_file" "$TARGET"
-    ensure chmod u+x "$_file"
-
-    if [ ! -x "$_file" ]; then
-        printf '%s\n' "Cannot execute $_file (likely because of mounting /tmp as noexec)." 1>&2
-        printf '%s\n' "Please copy the file to a location where you can execute binaries and run ./svt-agent-init." 1>&2
-        exit 1
-    fi
-
-    ignore "$_file" "$@"
-    _retval=$?
-
-    ignore rm "$_file"
-    ignore rmdir "$_dir"
-
-    return "$_retval"
+check_cmd() {
+    command -v "$1" > /dev/null 2>&1
 }
 
 say() {
@@ -138,16 +117,6 @@ say() {
 err() {
     say "$1" >&2
     exit 1
-}
-
-need_cmd() {
-    if ! check_cmd "$1"; then
-        err "need '$1' (command not found)"
-    fi
-}
-
-check_cmd() {
-    command -v "$1" > /dev/null 2>&1
 }
 
 # Run a command that should never fail. If the command fails execution
@@ -164,28 +133,6 @@ ignore() {
     "$@"
 }
 
-# This wraps curl or wget. Try curl first, if not installed,
-# use wget instead.
-downloader() {
-    if check_cmd curl; then
-        program=curl
-    elif check_cmd wget; then
-        program=wget
-    else
-        program='curl or wget' # to be used in error message of need_cmd
-    fi
-
-    if [ "$1" = --check ]; then
-        need_cmd "$program"
-    elif [ "$program" = curl ]; then
-        curl -sSfL "$1" -o "$2"
-    elif [ "$program" = wget ]; then
-        wget "$1" -O "$2"
-    else
-        err "Unknown downloader"   # should not reach here
-    fi
-}
-
-main "$@" || exit 1
-
-} # this ensures the entire script is downloaded #
+# wrapped up in a function so that we have some protection against only getting
+# half the file during "curl | sh"
+do_install
