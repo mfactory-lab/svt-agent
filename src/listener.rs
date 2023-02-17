065e5cd865ea001568494918fdf948fdd35a0c97
tiamo <vk.tiamo@gmail.com>
2023-02-17 15:11:38 +0200

    fix: listener, add `parse_logs`, add test

--- src/listener.rs
+++ src/listener.rs
@@ -1,4 +1,6 @@
+use crate::messenger::NewMessageEvent;
 use anchor_client::solana_client::nonblocking::pubsub_client::{PubsubClient, PubsubClientResult};
+use anchor_client::solana_client::rpc_client::RpcClient;
 use anchor_client::solana_client::rpc_config::{
     RpcTransactionLogsConfig, RpcTransactionLogsFilter,
 };
@@ -52,63 +54,68 @@ impl<'a> Listener<'a> {
             .await
     }
 
-    pub fn on<T: Event + AnchorDeserialize>(
-        &self,
-        log: &Response<RpcLogsResponse>,
-        f: &impl Fn(T),
-    ) -> &Self {
-        let mut logs = &log.value.logs[..];
-        let self_program_str = self.program_id.to_string();
-
-        if !logs.is_empty() {
-            debug!("Try to parse logs...");
-            if let Ok(mut execution) = Execution::new(&mut logs) {
-                debug!("{} logs found...", logs.len());
-                for l in logs {
-                    // Parse the log.
-                    let (event, new_program, did_pop) = {
-                        if self_program_str == execution.program() {
-                            handle_program_log::<T>(self_program_str.as_str(), l).unwrap_or_else(
-                                |e| {
-                                    warn!("Unable to parse log: {}", e);
-                                    (None, None, false)
-                                },
-                            )
-                        } else {
-                            let (program, did_pop) =
-                                handle_system_log(self_program_str.as_str(), l);
-                            (None, program, did_pop)
-                        }
-                    };
-                    // Emit the event.
-                    if let Some(e) = event {
-                        f(e);
-                    }
-                    // Switch program context on CPI.
-                    if let Some(new_program) = new_program {
-                        execution.push(new_program);
-                    }
-                    // Program returned.
-                    if did_pop {
-                        execution.pop();
-                    }
-                }
-            }
-        }
-
+    pub fn on<T: Event>(&self, log: &Response<RpcLogsResponse>, f: &impl Fn(T)) -> &Self {
+        parse_logs::<T>(self.program_id.to_string(), log.to_owned().value.logs, f);
         self
     }
 }
 
-fn handle_program_log<T: Event + AnchorDeserialize>(
+#[inline]
+fn parse_logs<T: Event>(program_id: String, logs: Vec<String>, f: &impl Fn(T)) {
+    let mut logs = &logs[..];
+    if !logs.is_empty() {
+        debug!("Try to parse logs...");
+        if let Ok(mut execution) = Execution::new(&mut logs) {
+            debug!("{} logs found...", logs.len());
+            for l in logs {
+                // Parse the log.
+                let (event, new_program, did_pop) = {
+                    if program_id == execution.program() {
+                        handle_program_log::<T>(program_id.as_str(), l).unwrap_or_else(|e| {
+                            warn!("Unable to parse log: {} ({})", l, e);
+                            (None, None, false)
+                        })
+                    } else {
+                        let (program, did_pop) = handle_system_log(program_id.as_str(), l);
+                        (None, program, did_pop)
+                    }
+                };
+                // Emit the event.
+                if let Some(e) = event {
+                    f(e);
+                }
+                // Switch program context on CPI.
+                if let Some(new_program) = new_program {
+                    execution.push(new_program);
+                }
+                // Program returned.
+                if did_pop {
+                    execution.pop();
+                }
+            }
+        }
+    }
+}
+
+const PROGRAM_LOG: &str = "Program log: ";
+const PROGRAM_DATA: &str = "Program data: ";
+
+fn handle_program_log<T: anchor_lang::Event + anchor_lang::AnchorDeserialize>(
     self_program_str: &str,
     l: &str,
 ) -> Result<(Option<T>, Option<String>, bool), ClientError> {
     // Log emitted from the current program.
-    if l.starts_with("Program log:") {
-        let log = l.to_string().split_off("Program log: ".len());
-        let borsh_bytes = anchor_lang::__private::base64::decode(log)
-            .map_err(|_| ClientError::LogParseError(l.to_string()))?;
+    if let Some(log) = l
+        .strip_prefix(PROGRAM_LOG)
+        .or_else(|| l.strip_prefix(PROGRAM_DATA))
+    {
+        let borsh_bytes = match anchor_lang::__private::base64::decode(log) {
+            Ok(borsh_bytes) => borsh_bytes,
+            _ => {
+                warn!("Could not base64 decode log: {}", log);
+                return Ok((None, None, false));
+            }
+        };
 
         let mut slice: &[u8] = &borsh_bytes[..];
         let disc: [u8; 8] = {
@@ -133,7 +140,7 @@ fn handle_program_log<T: Event + AnchorDeserialize>(
 }
 
 fn handle_system_log(this_program_str: &str, log: &str) -> (Option<String>, bool) {
-    if log.starts_with(&format!("Program {} log:", this_program_str)) {
+    if log.starts_with(&format!("Program {this_program_str} log:")) {
         (Some(this_program_str.to_string()), false)
     } else if log.contains("invoke") {
         (Some("cpi".to_string()), false) // Any string will do.
@@ -182,5 +189,44 @@ impl Execution {
     pub fn pop(&mut self) {
         assert!(!self.stack.is_empty());
         self.stack.pop().unwrap();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::constants::MESSENGER_PROGRAM_ID;
+    use crate::messenger::{DeleteMessageEvent, UpdateMessageEvent};
+    use anchor_client::solana_sdk::signature::Signature;
+    use std::str::FromStr;
+
+    #[test]
+    fn test1() {
+        // let client = RpcClient::new("https://api.testnet.solana.com");
+        // let sig = Signature::from_str("48rf44gyATMKVjnzaf2hP3k5SS7q7Qj8YqQxiMxtHURJJNEHrd7fzoerKjejbDag49aSNTfDwPwQ7KSeWNtQxA1N").unwrap();
+        // let tx = client
+        //     .get_transaction_with_config(&sig, Default::default())
+        //     .unwrap();
+
+        let program_id = MESSENGER_PROGRAM_ID;
+
+        let logs = vec![
+            "Program 4AnSBTc21f4wTBHmnFyarbosr28Qk4CgGFBHcRh4kYPw invoke [1]".to_string(),
+            "Program log: Instruction: PostMessage".to_string(),
+            "Program data: f+oFlw98HI/XJ7lRjgJLxQEwYGUh2xIw9QZIsLuauTpvLXnjZfTp0QQAAAAAAAAAIA85nxmXZazQMMhPkyUosaAQ2L5bWdpx10IvN+LP+WYlOO9jAAAAAAGQAAAAUHZUZm11UzNBSkRtTzJCTVZpTW1rdCtEVlYvRDJQTkwzRG5zcUl6Q1FaVlBQdnNOUU1CaGdtTmVwR1hJWjBPeVhSMkZTVlcyRkxKWlV4eHIycjNDVnFPaVFRYUV6bkNpMm9sdkluUFBuNHVQdXE3R0pVN0xBRVREMFFKMXpDVFFibzVudXU0TE9hcHZ5MzRt".to_string(),
+            "Program 4AnSBTc21f4wTBHmnFyarbosr28Qk4CgGFBHcRh4kYPw consumed 19775 of 200000 compute units".to_string(),
+            "Program 4AnSBTc21f4wTBHmnFyarbosr28Qk4CgGFBHcRh4kYPw success".to_string(),
+        ];
+
+        parse_logs::<NewMessageEvent>(program_id.to_string(), logs.clone(), &|e| {
+            println!("{:?}", e);
+            assert_eq!(e.message.id, 4)
+        });
+        parse_logs::<UpdateMessageEvent>(program_id.to_string(), logs.clone(), &|e| {
+            println!("{:?}", e);
+        });
+        parse_logs::<DeleteMessageEvent>(program_id.to_string(), logs, &|e| {
+            println!("{:?}", e);
+        });
     }
 }
