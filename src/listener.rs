1c9511e8411933b80789255dd31fadee68b5e3c9
tiamo <vk.tiamo@gmail.com>
2023-08-09 02:04:39 +0300

    fix: listen events

--- src/listener.rs
+++ src/listener.rs
@@ -1,17 +1,22 @@
 use crate::messenger::NewMessageEvent;
-use anchor_client::solana_client::nonblocking::pubsub_client::{PubsubClient, PubsubClientError, PubsubClientResult};
-use anchor_client::solana_client::rpc_client::RpcClient;
-use anchor_client::solana_client::rpc_config::{RpcTransactionLogsConfig, RpcTransactionLogsFilter};
-use anchor_client::solana_client::rpc_response::{Response, RpcLogsResponse};
-use anchor_client::solana_sdk::commitment_config::CommitmentConfig;
-use anchor_client::solana_sdk::pubkey::Pubkey;
 use anchor_client::ClientError;
-use anchor_lang::AnchorDeserialize;
-use anchor_lang::Event;
+use anchor_lang::{AnchorDeserialize, Event};
 use anyhow::Result;
+use clap::builder::Str;
 use futures::future::BoxFuture;
 use futures::stream::BoxStream;
-use regex::Regex;
+use futures::StreamExt;
+use regex::internal::Input;
+use regex::{Captures, Regex};
+use solana_client::nonblocking::pubsub_client::{PubsubClient, PubsubClientError, PubsubClientResult};
+use solana_client::rpc_client::RpcClient;
+use solana_client::rpc_config::{RpcTransactionLogsConfig, RpcTransactionLogsFilter};
+use solana_client::rpc_response::{Response, RpcLogsResponse};
+use solana_sdk::commitment_config::CommitmentConfig;
+use solana_sdk::pubkey::Pubkey;
+use std::borrow::Cow;
+use std::cell::OnceCell;
+use std::sync::OnceLock;
 use std::time::Duration;
 use tracing::{debug, info, warn};
 
@@ -46,52 +51,31 @@ impl<'a> Listener<'a> {
 
         tokio::select! {
             res = task => {
+                info!("Subscribed!");
                 res
             }
             _ = tokio::time::sleep(timeout) => {
-                Err(PubsubClientError::ConnectionClosed(format!("Timeout exceeded: {}s", timeout.as_secs())))
+                Err(PubsubClientError::ConnectionClosed(format!("timeout exceeded: {}s", timeout.as_secs())))
             }
         }
     }
 
     pub fn on<T: Event>(&self, log: &Response<RpcLogsResponse>, f: &impl Fn(T)) -> &Self {
-        parse_logs::<T>(self.program_id.to_string(), log.to_owned().value.logs, f);
+        parse_logs::<T>(&self.program_id.to_string(), &log.value.logs, f);
         self
     }
 }
 
 #[inline]
-fn parse_logs<T: Event>(program_id: String, logs: Vec<String>, f: &impl Fn(T)) {
-    let mut logs = &logs[..];
-    if !logs.is_empty() {
-        debug!("Try to parse logs...");
-        if let Ok(mut execution) = Execution::new(&mut logs) {
-            debug!("{} logs found...", logs.len());
-            for l in logs {
-                // Parse the log.
-                let (event, new_program, did_pop) = {
-                    if program_id == execution.program() {
-                        handle_program_log::<T>(program_id.as_str(), l).unwrap_or_else(|e| {
-                            warn!("Unable to parse log: {} ({})", l, e);
-                            (None, None, false)
-                        })
-                    } else {
-                        let (program, did_pop) = handle_system_log(program_id.as_str(), l);
-                        (None, program, did_pop)
-                    }
-                };
-                // Emit the event.
-                if let Some(e) = event {
-                    f(e);
-                }
-                // Switch program context on CPI.
-                if let Some(new_program) = new_program {
-                    execution.push(new_program);
-                }
-                // Program returned.
-                if did_pop {
-                    execution.pop();
-                }
+fn parse_logs<E: Event>(program_id_str: &str, logs: impl IntoIterator<Item = impl Into<String>>, f: &impl Fn(E)) {
+    for (pid, log) in Execution::new(logs) {
+        if program_id_str == pid {
+            let (event, _, _) = handle_program_log::<E>(program_id_str, &log).unwrap_or_else(|e| {
+                warn!("Unable to parse log: {} ({})", log, e);
+                (None, None, false)
+            });
+            if let Some(e) = event {
+                f(e);
             }
         }
     }
@@ -99,6 +83,9 @@ fn parse_logs<T: Event>(program_id: String, logs: Vec<String>, f: &impl Fn(T)) {
 
 const PROGRAM_LOG: &str = "Program log: ";
 const PROGRAM_DATA: &str = "Program data: ";
+
+static PROGRAM_INVOKE_RE: OnceLock<Regex> = OnceLock::new();
+static PROGRAM_SUCCESS_RE: OnceLock<Regex> = OnceLock::new();
 
 #[inline]
 fn handle_program_log<T: anchor_lang::Event + anchor_lang::AnchorDeserialize>(
@@ -144,7 +131,7 @@ fn handle_system_log(this_program_str: &str, log: &str) -> (Option<String>, bool
     } else if log.contains("invoke") {
         (Some("cpi".to_string()), false) // Any string will do.
     } else {
-        let re = Regex::new(r"^Program (.*) success*$").unwrap();
+        let re = PROGRAM_SUCCESS_RE.get_or_init(|| Regex::new(r"^Program (.*) success*$").unwrap());
         if re.is_match(log) {
             (None, true)
         } else {
@@ -153,42 +140,40 @@ fn handle_system_log(this_program_str: &str, log: &str) -> (Option<String>, bool
     }
 }
 
+#[derive(Debug)]
 struct Execution {
-    stack: Vec<String>,
+    logs: Vec<String>,
+    program_id: String,
+    index: usize,
+}
+
+impl Iterator for Execution {
+    type Item = (String, String);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let re = PROGRAM_INVOKE_RE.get_or_init(|| Regex::new(r"^Program (.*) invoke.*$").unwrap());
+        if let Some(log) = self.logs.get(self.index) {
+            if let Some(c) = re.captures(log).and_then(|l| l.get(1)) {
+                self.program_id = c.as_str().to_owned();
+            }
+            self.index += 1;
+            Some((self.program_id.to_owned(), log.to_owned()))
+        } else {
+            None
+        }
+    }
 }
 
 impl Execution {
-    pub fn new(logs: &mut &[String]) -> Result<Self, ClientError> {
-        let l = &logs[0];
-        *logs = &logs[1..];
-
-        let re = Regex::new(r"^Program (.*) invoke.*$").unwrap();
-        let c = re
-            .captures(l)
-            .ok_or_else(|| ClientError::LogParseError(l.to_string()))?;
-        let program = c
-            .get(1)
-            .ok_or_else(|| ClientError::LogParseError(l.to_string()))?
-            .as_str()
-            .to_string();
-        Ok(Self { stack: vec![program] })
-    }
-
-    pub fn program(&self) -> String {
-        // assert!(!self.stack.is_empty());
-        if self.stack.is_empty() {
-            return Default::default();
-        }
-        self.stack[self.stack.len() - 1].clone()
-    }
-
-    pub fn push(&mut self, new_program: String) {
-        self.stack.push(new_program);
-    }
-
-    pub fn pop(&mut self) {
-        if !self.stack.is_empty() {
-            self.stack.pop().unwrap();
+    pub fn new<T, I>(logs: T) -> Self
+    where
+        I: Into<String>,
+        T: IntoIterator<Item = I>,
+    {
+        Self {
+            logs: logs.into_iter().map(|i| i.into()).collect(),
+            program_id: Default::default(),
+            index: 0,
         }
     }
 }
@@ -202,32 +187,64 @@ mod tests {
     use std::str::FromStr;
 
     #[test]
-    fn test1() {
-        // let client = RpcClient::new("https://api.testnet.solana.com");
-        // let sig = Signature::from_str("48rf44gyATMKVjnzaf2hP3k5SS7q7Qj8YqQxiMxtHURJJNEHrd7fzoerKjejbDag49aSNTfDwPwQ7KSeWNtQxA1N").unwrap();
-        // let tx = client
-        //     .get_transaction_with_config(&sig, Default::default())
-        //     .unwrap();
-
+    fn can_parse_simple_new_message_log() {
         let program_id = MESSENGER_PROGRAM_ID;
 
         let logs = vec![
-            "Program CgRaMXqqRHNT3Zo2uVZfX72TuxUgcLb8E3A8KrXnbXAC invoke [1]".to_string(),
-            "Program log: Instruction: PostMessage".to_string(),
-            "Program data: f+oFlw98HI/XJ7lRjgJLxQEwYGUh2xIw9QZIsLuauTpvLXnjZfTp0QQAAAAAAAAAIA85nxmXZazQMMhPkyUosaAQ2L5bWdpx10IvN+LP+WYlOO9jAAAAAAGQAAAAUHZUZm11UzNBSkRtTzJCTVZpTW1rdCtEVlYvRDJQTkwzRG5zcUl6Q1FaVlBQdnNOUU1CaGdtTmVwR1hJWjBPeVhSMkZTVlcyRkxKWlV4eHIycjNDVnFPaVFRYUV6bkNpMm9sdkluUFBuNHVQdXE3R0pVN0xBRVREMFFKMXpDVFFibzVudXU0TE9hcHZ5MzRt".to_string(),
-            "Program CgRaMXqqRHNT3Zo2uVZfX72TuxUgcLb8E3A8KrXnbXAC consumed 19775 of 200000 compute units".to_string(),
-            "Program CgRaMXqqRHNT3Zo2uVZfX72TuxUgcLb8E3A8KrXnbXAC success".to_string(),
+            "Program CgRaMXqqRHNT3Zo2uVZfX72TuxUgcLb8E3A8KrXnbXAC invoke [1]",
+            "Program log: Instruction: PostMessage",
+            "Program data: f+oFlw98HI/XJ7lRjgJLxQEwYGUh2xIw9QZIsLuauTpvLXnjZfTp0QQAAAAAAAAAIA85nxmXZazQMMhPkyUosaAQ2L5bWdpx10IvN+LP+WYlOO9jAAAAAAGQAAAAUHZUZm11UzNBSkRtTzJCTVZpTW1rdCtEVlYvRDJQTkwzRG5zcUl6Q1FaVlBQdnNOUU1CaGdtTmVwR1hJWjBPeVhSMkZTVlcyRkxKWlV4eHIycjNDVnFPaVFRYUV6bkNpMm9sdkluUFBuNHVQdXE3R0pVN0xBRVREMFFKMXpDVFFibzVudXU0TE9hcHZ5MzRt",
+            "Program CgRaMXqqRHNT3Zo2uVZfX72TuxUgcLb8E3A8KrXnbXAC consumed 19775 of 200000 compute units",
+            "Program CgRaMXqqRHNT3Zo2uVZfX72TuxUgcLb8E3A8KrXnbXAC success",
         ];
 
-        parse_logs::<NewMessageEvent>(program_id.to_string(), logs.clone(), &|e| {
-            println!("{:?}", e);
-            assert_eq!(e.message.id, 4)
+        parse_logs::<NewMessageEvent>(program_id, logs.clone(), &|e| {
+            // println!("{:?}", e);
+            assert_eq!(e.message.id, 4);
         });
-        parse_logs::<UpdateMessageEvent>(program_id.to_string(), logs.clone(), &|e| {
-            println!("{:?}", e);
+    }
+
+    #[test]
+    fn can_parse_new_message_log() {
+        let program_id = MESSENGER_PROGRAM_ID;
+
+        let logs = vec![
+            "Program ComputeBudget111111111111111111111111111111 invoke [1]",
+            "Program ComputeBudget111111111111111111111111111111 success",
+            "Program ComputeBudget111111111111111111111111111111 invoke [1]",
+            "Program ComputeBudget111111111111111111111111111111 success",
+            "Program CgRaMXqqRHNT3Zo2uVZfX72TuxUgcLb8E3A8KrXnbXAC invoke [1]",
+            "Program log: Instruction: PostMessage",
+            "Program data: f+oFlw98HI/RQ5wPA9epyN8cp7mjF0G0EywpKJ1Ft5g7ccc2hGxpmgIAAAAAAAAA1LvJJMGaavwqOeBSMUJ5c+9JFJJvqCCT1UkIywNvmieXmNJkAAAAAAFMAAAAWXFqY1V0OU1uelg1VFF1T05aY2dPUEd0aU5mWU8rN1krbkZZRTdrZzRvMU1xR0g3YUFpRFNqbExCdlJscWZtODFtbmxwOWo2NXhrPQ==",
+            "Program CgRaMXqqRHNT3Zo2uVZfX72TuxUgcLb8E3A8KrXnbXAC consumed 14281 of 200000 compute units",
+            "Program CgRaMXqqRHNT3Zo2uVZfX72TuxUgcLb8E3A8KrXnbXAC success"
+        ];
+
+        parse_logs::<NewMessageEvent>(program_id, logs, &|e| {
+            // println!("{:?}", e);
+            assert_eq!(e.message.id, 2)
         });
-        parse_logs::<DeleteMessageEvent>(program_id.to_string(), logs, &|e| {
-            println!("{:?}", e);
+    }
+
+    #[test]
+    fn can_parse_delete_message_log() {
+        let program_id = MESSENGER_PROGRAM_ID;
+
+        let logs = vec![
+            "Program ComputeBudget111111111111111111111111111111 invoke [1]",
+            "Program ComputeBudget111111111111111111111111111111 success",
+            "Program ComputeBudget111111111111111111111111111111 invoke [1]",
+            "Program ComputeBudget111111111111111111111111111111 success",
+            "Program CgRaMXqqRHNT3Zo2uVZfX72TuxUgcLb8E3A8KrXnbXAC invoke [1]",
+            "Program log: Instruction: DeleteMessage",
+            "Program data: gsiCEypVahHRQ5wPA9epyN8cp7mjF0G0EywpKJ1Ft5g7ccc2hGxpmtS7ySTBmmr8KjngUjFCeXPvSRSSb6ggk9VJCMsDb5onAwAAAAAAAAA=",
+            "Program CgRaMXqqRHNT3Zo2uVZfX72TuxUgcLb8E3A8KrXnbXAC consumed 13928 of 200000 compute units",
+            "Program CgRaMXqqRHNT3Zo2uVZfX72TuxUgcLb8E3A8KrXnbXAC success",
+        ];
+
+        parse_logs::<DeleteMessageEvent>(program_id, logs, &|e| {
+            // println!("{:?}", e);
+            assert_eq!(e.id, 3);
         });
     }
 }
